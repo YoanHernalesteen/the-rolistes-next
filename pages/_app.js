b4e596210c468cdd1c9f1f3e24fe5af4f071668a
Neetsel <55947537+Neetsel@users.noreply.github.com>
2022-12-30 18:50:10 +0100

    More updates to try to get this thing working

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,24 @@ import { useEffect } from "react";
 import * as actions from "../store/actions/index";
 import { withRouter } from "next/router";
 import RouteChangeTracker from "../tracking/RouteChangeTracker";
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from '../store/reducers/posts';
+import globalReducer from '../store/reducers/global';
+import thunk from 'redux-thunk';
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
+
+const rootReducer = combineReducers({
+  posts: postsReducer,
+  global: globalReducer
+})
+
+const store =  createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 function MyApp({ Component, pageProps }) {
   const { onFetchPosts, onSetShowPopup } = props;
@@ -18,24 +35,26 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <Layout>
-      <RouteChangeTracker/>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <RouteChangeTracker />
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-      onFetchPosts: () => dispatch (actions.fetchPosts()),
-      onSetShowPopup: (showPopup) => dispatch (actions.setShowPopup(showPopup)) 
+    onFetchPosts: () => dispatch(actions.fetchPosts()),
+    onSetShowPopup: (showPopup) => dispatch(actions.setShowPopup(showPopup)),
   };
 };
 
 const mapStateToProps = (state) => {
   return {
-      showPopup: state.global.showPopup
+    showPopup: state.global.showPopup,
   };
 };
 
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(MyApp));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MyApp));
